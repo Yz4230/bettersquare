a9e1889000bf79fac2dc28b1e291d8268f072a55
Yz4230 <yuzupon1480@gmail.com>
2021-04-18 21:54:21 +0900

    chore: refactor

--- src/injectors/SideMenuInjector.ts
+++ src/injectors/SideMenuInjector.ts
@@ -1,18 +1,22 @@
 import { InjectorBase } from "./InjectorBase";
 
+const overrideTitleBar = () => {
+  const titleBar = document.querySelector(
+    "body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td"
+  );
+  titleBar.innerHTML = "";
+  const additional = document.createElement("h1");
+  additional.textContent = "Better Square";
+  additional.style.textAlign = "center";
+  additional.style.color = "blue";
+  titleBar.appendChild(additional);
+};
+
 export class SideMenuInjector extends InjectorBase {
   match(): boolean {
     return Boolean(document.querySelector("body > .menu-outer"));
   }
   run(): void {
-    const titleBar = document.querySelector(
-      "body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td"
-    );
-    titleBar.innerHTML = "";
-    const additional = document.createElement("h1");
-    additional.textContent = "Better Square";
-    additional.style.textAlign = "center";
-    additional.style.color = "blue";
-    titleBar.appendChild(additional);
+    overrideTitleBar();
   }
 }
